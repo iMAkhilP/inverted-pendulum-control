bbab06cd58378c7a8d8cffb4adc7ccabe4e5875b
iMAkhilP <patelakhil1998@outlook.com>
2025-03-07 00:32:18 +0100

    cart_pendulum function defined

--- cartpendulum.py
+++ cartpendulum.py
@@ -1,32 +1,35 @@
-"""
-cart-pendulum model
-# M             -   mass of cart
-# m             -   mass of pendulum
-# g             -   gravitational acceleration
-# l             -   length of pendulum
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+
+#cart-pendulum model
+M = 1.0     # mass of cart (kg)
+m = 0.5     # mass of pendulum (kg)
+g = 9.81    # gravitational acceleration (m/s^2)
+l = 1.0     # length of pendulum (m)
+b_c = 0.35  # Damping coefficient for cart (kg*m^2/s)
+b_c = 0.1   # Damping coefficient for pendulum (kg*m^2/s)
 # x             -   displacement of cart
 # theta         -   angular displacement of cart
 # x_dot         -   velocity of cart
 # theta_dot     -   angular velocity of pendulum
 # x_Ddot        -   acceleration of cart
 # theta_Ddot    -   angular acceleration of pendulum
-# U             -   force acting on cart (control force)
-"""
 
-"""
-for equations of motion
+# simulation parameter
+U = lambda time: 0.0    -   # force (N) acting on cart (control force) is function of time
 
-Langrangian approach is used
-L = K - U
-where L is Langrangian
-      K is kinetic energy
-      U is potential energy
-"""
+def cart_pendulum(state, time):
+    x, dx, theta, dtheta = state
+    
+    A = np.array([M+m, -m*l*np.cos(theta)],
+                 [-m*l*np.cos(theta), m*l**2])
+    
+    b = np.array([(U - (m*l*np.sin(theta)*dtheta**2) - (b_c*dx)),
+                  ((m*g*l*np.sin(theta)) - (b_p*dtheta))])
+    
+    ddx, ddtheta = np.linalg.solve(A,B)
+    
+    return [dx, ddx, dtheta, ddtheta]
 
-"""
-generalized coordinates - q
-q = (x,theta)
-
-K for cart and pendulum
-U for cart and pendulum
-"""
+initial_state = [0, 0, np.pi/4, 0.5]
